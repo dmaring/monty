f947cf3f8ee0422c1ce678e1e4c1d61efe1345bb
scurry222 <scurry222@gmail.com>
2019-05-16 19:27:23 +0000

    removing whitespace

--- op_helpers_2.c
+++ op_helpers_2.c
@@ -8,27 +8,27 @@
 
 void op_add(stack_t **head, unsigned int line_number)
 {
-        stack_t *new = NULL, *node_0 = NULL, *node_1 = NULL;
-        int sum = 0;
+	stack_t *new = NULL, *node_0 = NULL, *node_1 = NULL;
+	int sum = 0;
 
-        if (dlistint_len(*head) < 2)
-        {
-                line_number = line_number;
-                /*error message, not enough elements*/
-        }
-        node_0 = get_dnodeint_at_index(*head, 0);
-        node_1 = get_dnodeint_at_index(*head, 1);
+	if (dlistint_len(*head) < 2)
+	{
+		line_number = line_number;
+		/*error message, not enough elements*/
+	}
+	node_0 = get_dnodeint_at_index(*head, 0);
+	node_1 = get_dnodeint_at_index(*head, 1);
 
-        sum = node_0->n + node_1->n;
-        delete_dnodeint_at_index(head, 0);
-        delete_dnodeint_at_index(head, 0);
+	sum = node_0->n + node_1->n;
+	delete_dnodeint_at_index(head, 0);
+	delete_dnodeint_at_index(head, 0);
 
-        new =  add_dnodeint(head, sum);
-        if (!new)
-        {
-                line_number = line_number;
-                /*error message, malloc fail */
-        }
+	new =  add_dnodeint(head, sum);
+	if (!new)
+	{
+		line_number = line_number;
+		/*error message, malloc fail */
+	}
 }
 
 /**
@@ -38,27 +38,27 @@ void op_add(stack_t **head, unsigned int line_number)
 */
 void op_sub(stack_t **head, unsigned int line_number)
 {
-        stack_t *new = NULL, *node_0 = NULL, *node_1 = NULL;
-        int sum = 0;
+	stack_t *new = NULL, *node_0 = NULL, *node_1 = NULL;
+	int sum = 0;
 
-        if (dlistint_len(*head) < 2)
-        {
-                /*error message, not enough elements*/
-                line_number = line_number;
-        }
-        node_0 = get_dnodeint_at_index(*head, 0);
-        node_1 = get_dnodeint_at_index(*head, 1);
+	if (dlistint_len(*head) < 2)
+	{
+		/*error message, not enough elements*/
+		line_number = line_number;
+	}
+	node_0 = get_dnodeint_at_index(*head, 0);
+	node_1 = get_dnodeint_at_index(*head, 1);
 
-        sum = node_0->n - node_1->n;
-        delete_dnodeint_at_index(head, 0);
-        delete_dnodeint_at_index(head, 0);
+	sum = node_0->n - node_1->n;
+	delete_dnodeint_at_index(head, 0);
+	delete_dnodeint_at_index(head, 0);
 
-        new =  add_dnodeint(head, sum);
-        if (!new)
-        {
-                /*error message, malloc fail */
-                line_number = line_number;
-        }
+	new =  add_dnodeint(head, sum);
+	if (!new)
+	{
+		/*error message, malloc fail */
+		line_number = line_number;
+	}
 }
 
 /**
@@ -70,57 +70,57 @@ void op_sub(stack_t **head, unsigned int line_number)
 
 void op_mul(stack_t **head, unsigned int line_number)
 {
-        stack_t *new = NULL, *node_0 = NULL, *node_1 = NULL;
-        int prod = 0;
+	stack_t *new = NULL, *node_0 = NULL, *node_1 = NULL;
+	int prod = 0;
 
-        if (dlistint_len(*head) < 2)
-        {
-                line_number = line_number;
-                /*error message, not enough elements*/
-        }
-        node_0 = get_dnodeint_at_index(*head, 0);
-        node_1 = get_dnodeint_at_index(*head, 1);
+	if (dlistint_len(*head) < 2)
+	{
+		line_number = line_number;
+		/*error message, not enough elements*/
+	}
+	node_0 = get_dnodeint_at_index(*head, 0);
+	node_1 = get_dnodeint_at_index(*head, 1);
 
-        prod = node_0->n * node_1->n;
-        delete_dnodeint_at_index(head, 0);
-        delete_dnodeint_at_index(head, 0);
+	prod = node_0->n * node_1->n;
+	delete_dnodeint_at_index(head, 0);
+	delete_dnodeint_at_index(head, 0);
 
-        new =  add_dnodeint(head, prod);
-        if (!new)
-        {
-                line_number = line_number;
-                /*error message, malloc fail */
-        }
+	new =  add_dnodeint(head, prod);
+	if (!new)
+	{
+		line_number = line_number;
+		/*error message, malloc fail */
+	}
 }
 
 void op_div(stack_t **head, unsigned int line_number)
 {
-        stack_t *new = NULL, *node_0 = NULL, *node_1 = NULL;
-        int sum = 0;
+	stack_t *new = NULL, *node_0 = NULL, *node_1 = NULL;
+	int sum = 0;
 
-        if (dlistint_len(*head) < 2)
-        {
-                line_number = line_number;
-                /*error message, not enough elements*/
-        }
-        if (node_0->n == 0)
-        {
-                line_number = line_number;
-                /*error message, cant div by zero */
-        }
-        node_0 = get_dnodeint_at_index(*head, 0);
-        node_1 = get_dnodeint_at_index(*head, 1);
+	if (dlistint_len(*head) < 2)
+	{
+		line_number = line_number;
+		/*error message, not enough elements*/
+	}
+	if (node_0->n == 0)
+	{
+		line_number = line_number;
+		/*error message, cant div by zero */
+	}
+	node_0 = get_dnodeint_at_index(*head, 0);
+	node_1 = get_dnodeint_at_index(*head, 1);
 
-        sum = node_0->n / node_1->n;
-        delete_dnodeint_at_index(head, 0);
-        delete_dnodeint_at_index(head, 0);
+	sum = node_0->n / node_1->n;
+	delete_dnodeint_at_index(head, 0);
+	delete_dnodeint_at_index(head, 0);
 
-        new =  add_dnodeint(head, sum);
-        if (!new)
-        {
-                /*error message, malloc fail */
-                line_number = line_number;
-        }
+	new =  add_dnodeint(head, sum);
+	if (!new)
+	{
+		/*error message, malloc fail */
+		line_number = line_number;
+	}
 }
 
 /**
@@ -132,25 +132,25 @@ void op_div(stack_t **head, unsigned int line_number)
 
 void op_mod(stack_t **head, unsigned int line_number)
 {
-        stack_t *new = NULL, *node_0 = NULL, *node_1 = NULL;
-        int sum = 0;
+	stack_t *new = NULL, *node_0 = NULL, *node_1 = NULL;
+	int sum = 0;
 
-        if (dlistint_len(*head) < 2)
-        {
-                line_number = line_number;
-                /*error message, not enough elements*/
-        }
-        node_0 = get_dnodeint_at_index(*head, 0);
-        node_1 = get_dnodeint_at_index(*head, 1);
+	if (dlistint_len(*head) < 2)
+	{
+		line_number = line_number;
+		/*error message, not enough elements*/
+	}
+	node_0 = get_dnodeint_at_index(*head, 0);
+	node_1 = get_dnodeint_at_index(*head, 1);
 
-        sum = node_0->n % node_1->n;
-        delete_dnodeint_at_index(head, 0);
-        delete_dnodeint_at_index(head, 0);
+	sum = node_0->n % node_1->n;
+	delete_dnodeint_at_index(head, 0);
+	delete_dnodeint_at_index(head, 0);
 
-        new =  add_dnodeint(head, sum);
-        if (!new)
-        {
-                line_number = line_number;
-                /*error message, malloc fail */
-        }
+	new =  add_dnodeint(head, sum);
+	if (!new)
+	{
+		line_number = line_number;
+		/*error message, malloc fail */
+	}
 }
