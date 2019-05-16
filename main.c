fc23c009220cb5f4c02e1013261cce8dbc381649
Drew <andrew.maring@gmail.com>
2019-05-16 19:00:47 +0000

    header.h

--- main.c
+++ main.c
@@ -28,8 +28,8 @@ int main(int argc, char *argv[])
 		/* remove nl from line */
 		rm_nl(&line);
 		/* check if line is blank */
-		/* if (wordcount(line) == 0) */
-		/* 	break; */
+		if (wordcount(line) == 0)
+			break;
 		/* add line and line number to global_struct */
 		global_struct = create_global_struct(linenumber, line);
 		/* TEST only for global struct members */
